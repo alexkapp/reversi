dfc0d7c9c630ebd7d2733c08027e743791d26128
Alex Kappelmann <aekwdf@mail.umsl.edu>
2017-01-20 15:20:53 -0600

    Update RiskRegions.java

--- Reversi/src/ai/evaluation/RiskRegions.java
+++ Reversi/src/ai/evaluation/RiskRegions.java
@@ -6,7 +6,7 @@ package ai.evaluation;
 public class RiskRegions {
 	
 	public static final int [][] rrmap = new int [][]
-			{ { 5, 4, 3, 3, 3, 3, 4, 5 },
+		      { { 5, 4, 3, 3, 3, 3, 4, 5 },
 		      { 4, 4, 2, 2, 2, 2, 4, 4 },
 		      { 3, 2, 1, 1, 1, 1, 2, 3 },
 		      { 3, 2, 1, 1, 1, 1, 2, 3 },
@@ -17,7 +17,7 @@ public class RiskRegions {
 		      
     /* Stability rankings */
     public static final int [][] rankmap = new int [][]
-		{ { 5, 1, 4, 4, 4, 4, 1, 5 },
+	    { { 5, 1, 4, 4, 4, 4, 1, 5 },
 	      { 1, 1, 2, 2, 2, 2, 1, 1 },
 	      { 4, 2, 3, 3, 3, 3, 2, 4 },
 	      { 4, 2, 3, 3, 3, 3, 2, 4 },
